cb0f86709080d7805d743fcea53fd03312140dad
human97 <aleksey.n.popov@gmail.com>
2020-11-10 00:07:25 +0400

    add adaptive

--- dist/assets/js/app.js
+++ dist/assets/js/app.js
@@ -7,13 +7,16 @@ const allRadio = form.querySelectorAll('.plan')
 const count = () => {
     for (let i = 0; i < allRadio.length; i++) {
         if (allRadio[i].checked) {
+            // когда радио кнопка чек меняется цвет бэкграунда и бордера
             allRadio[i].parentNode.parentNode.style.backgroundColor = '#bfe1fd4d'
             allRadio[i].parentNode.parentNode.style.borderColor = '#c1d9fc'
+
             let planCost = allRadio[i].value // стоимость выбранной лицензии
             let license = form.querySelector('#numberSelect').value // кол-во лицензий
             let total = planCost * license // итоговая стоимость
             form.querySelector('.total_price').innerHTML = `&#36;${total}<sup>us</sup>` // вывод иоговой стоимости
 
+            // условия вывода выбранного плана лицензий
             if (planCost == 34) {
                 form.querySelector('#selPlanNum').textContent = '3'
             } else if (planCost == 22) {
@@ -30,7 +33,7 @@ const count = () => {
 
 count()
 
-
+// 
 form.addEventListener('click', function (e) {
     let clickCount = e.target.className
     if (clickCount === 'plan' || 'number_select') {
@@ -40,6 +43,6 @@ form.addEventListener('click', function (e) {
 
 
 document.getElementById('btn_byu').addEventListener('click', function (e) {
-    e.preventDefault();
+    e.preventDefault()
 })
 "use strict";
