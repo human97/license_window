83b3bbe8fc17f9695a88ac0ff39afb486fdfaa79
human97 <aleksey.n.popov@gmail.com>
2021-03-02 02:24:31 +0400

    add forEach

--- src/assets/js/components/license_window.js
+++ src/assets/js/components/license_window.js
@@ -5,13 +5,13 @@ const allRadio = form.querySelectorAll('.plan')
 
 // формула расчёта
 const count = () => {
-    for (let i = 0; i < allRadio.length; i++) {
-        if (allRadio[i].checked) {
+    allRadio.forEach((e) => {
+        if (e.checked) {
             // когда радио кнопка чек меняется цвет бэкграунда и бордера
-            allRadio[i].parentNode.parentNode.style.backgroundColor = '#bfe1fd4d'
-            allRadio[i].parentNode.parentNode.style.borderColor = '#c1d9fc'
+            e.parentNode.parentNode.style.backgroundColor = '#bfe1fd4d'
+            e.parentNode.parentNode.style.borderColor = '#c1d9fc'
 
-            let planCost = allRadio[i].value // стоимость выбранной лицензии
+            let planCost = e.value // стоимость выбранной лицензии
             let license = form.querySelector('#numberSelect').value // кол-во лицензий
             let total = planCost * license // итоговая стоимость
             form.querySelector('.total_price').innerHTML = `&#36;${total}<sup>us</sup>` // вывод иоговой стоимости
@@ -25,10 +25,10 @@ const count = () => {
                 form.querySelector('#selPlanNum').textContent = '1'
             }
         } else {
-            allRadio[i].parentNode.parentNode.style.backgroundColor = 'transparent'
-            allRadio[i].parentNode.parentNode.style.borderColor = 'transparent'
+            e.parentNode.parentNode.style.backgroundColor = 'transparent'
+            e.parentNode.parentNode.style.borderColor = 'transparent'
         }
-    }
+    })
 }
 
 count()
